c55dbef5e3b7a2802299e7c3f28f20d0f23dee23
Oleg Babenko <lokard.dev@gmail.com>
2022-03-23 21:12:09 +0200

    - initial migrations

---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,5 @@
+drop table list_dishes;
+drop table dish_goods;
+drop table lists;
+drop table dishes;
+drop table goods;
---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,54 @@
+create table goods
+(
+    id          serial
+        primary key,
+    created_at  timestamp with time zone,
+    updated_at  timestamp with time zone,
+    deleted_at  timestamp with time zone,
+    title       text,
+    description text
+);
+
+create table dishes
+(
+    id          serial
+        primary key,
+    created_at  timestamp with time zone,
+    updated_at  timestamp with time zone,
+    deleted_at  timestamp with time zone,
+    title       text,
+    description text
+);
+
+create table lists
+(
+    id          serial
+        primary key,
+    created_at  timestamp with time zone,
+    updated_at  timestamp with time zone,
+    deleted_at  timestamp with time zone,
+    title       text,
+    description text
+);
+
+create table dish_goods
+(
+    dish_id  int not null
+        constraint fk_dish_goods_dish
+            references dishes,
+    goods_id int not null
+        constraint fk_dish_goods_goods
+            references goods,
+    primary key (dish_id, goods_id)
+);
+
+create table list_dishes
+(
+    list_id int not null
+        constraint fk_list_dishes_list
+            references lists,
+    dish_id int not null
+        constraint fk_list_dishes_dish
+            references dishes,
+    primary key (list_id, dish_id)
+);
