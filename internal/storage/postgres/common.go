459319cbb83c3f4e37aa7330b36581e435d72222
Oleg Babenko <lokard.dev@gmail.com>
2022-03-31 13:56:05 +0300

    - lists crud

--- internal/storage/postgres/common.go
+++ internal/storage/postgres/common.go
@@ -1,5 +1,12 @@
 package postgres
 
+import (
+	"context"
+	"fmt"
+	"my_goods/internal/entity"
+	"my_goods/pkg/logger"
+)
+
 const (
 	DishTable      = "dishes"
 	DishGoodsTable = "dish_goods"
@@ -8,3 +15,40 @@ const (
 	ListToDishes   = "list_dishes"
 	ListToGoods    = "list_goods"
 )
+
+type Repository struct {
+	DB  PgxPoolInterface
+	Ctx context.Context
+}
+
+func (r *Repository) GetBaseGoodsInfo(id int32) (*[]entity.GoodsWithAmount, error) {
+	var goods []entity.GoodsWithAmount
+	query := fmt.Sprintf("SELECT goods.Id, goods.title, goods.description, dish_goods.amount FROM %s "+
+		"FULL JOIN %s ON dish_goods.goods_id = goods.id WHERE dish_id=$1", DishGoodsTable, GoodsTable)
+	rows, err := r.DB.Query(r.Ctx, query, id)
+	defer rows.Close()
+	for rows.Next() {
+		goodInfo := entity.GoodsWithAmount{}
+		if err = rows.Scan(&goodInfo.Id, &goodInfo.Title, &goodInfo.Description, &goodInfo.Amount); err != nil {
+			logger.Error(err)
+		}
+		goods = append(goods, goodInfo)
+	}
+	return &goods, err
+}
+
+func (r *Repository) GetBaseDishInfo(id int32) (*entity.Dish, error) {
+	var pgxDish entity.PgxDish
+	query := fmt.Sprintf("SELECT id, title, description FROM %s WHERE id=$1", DishTable)
+	rows, err := r.DB.Query(r.Ctx, query, id)
+	defer rows.Close()
+	for rows.Next() {
+		if err = rows.Scan(&pgxDish.Id, &pgxDish.Title, &pgxDish.Description); err != nil {
+			logger.Error(err)
+		}
+	}
+	if err != nil {
+		logger.Error(err)
+	}
+	return pgxDish.ToClean(), err
+}
